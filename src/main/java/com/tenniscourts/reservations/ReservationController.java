42f2a1ce8721e6a274aa55200030ec3e0fdfba4e
alexadz96 <86671204+alexadz96@users.noreply.github.com>
2021-06-29 19:30:03 +0300

    Update ReservationController.java

--- src/main/java/com/tenniscourts/reservations/ReservationController.java
+++ src/main/java/com/tenniscourts/reservations/ReservationController.java
@@ -3,6 +3,14 @@ package com.tenniscourts.reservations;
 import com.tenniscourts.config.BaseRestController;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/reservation")
 
 @AllArgsConstructor
 public class ReservationController extends BaseRestController {
