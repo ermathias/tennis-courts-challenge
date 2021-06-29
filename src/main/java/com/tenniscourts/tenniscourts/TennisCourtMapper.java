d0d3e593823825e8b06f43a4dffbec91da9f391b
alexadz96 <86671204+alexadz96@users.noreply.github.com>
2021-06-29 19:26:42 +0300

    Update TennisCourtMapper.java

--- src/main/java/com/tenniscourts/tenniscourts/TennisCourtMapper.java
+++ src/main/java/com/tenniscourts/tenniscourts/TennisCourtMapper.java
@@ -1,12 +1,16 @@
-package com.tenniscourts.tenniscourts;
+package com.tenniscourts.reservations;
 
 import org.mapstruct.InheritInverseConfiguration;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.springframework.stereotype.Component;
 
+@Component
 @Mapper(componentModel = "spring")
-public interface TennisCourtMapper {
-    TennisCourtDTO map(TennisCourt source);
-
-    @InheritInverseConfiguration
-    TennisCourt map(TennisCourtDTO source);
-}
+public interface ReservationMapper {
+Reservation map(ReservationDTO source);
+@InheritInverseConfiguration
+ReservationDTO map(Reservation source);
+@Mapping(target = "guest.id", source = "guestId")
+    @Mapping(target = "schedule.id", source = "scheduleId")
+    Reservation map(CreateReservationRequestDTO source);}
