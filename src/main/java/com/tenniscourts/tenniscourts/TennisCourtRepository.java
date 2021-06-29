cc7cb3054757be84b6c4699ea109d3f498c33e45
alexadz96 <86671204+alexadz96@users.noreply.github.com>
2021-06-29 19:25:05 +0300

    Update TennisCourtRepository.java

--- src/main/java/com/tenniscourts/tenniscourts/TennisCourtRepository.java
+++ src/main/java/com/tenniscourts/tenniscourts/TennisCourtRepository.java
@@ -1,6 +1,12 @@
-package com.tenniscourts.tenniscourts;
+package com.tenniscourts.reservations;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.time.LocalDateTime;
+import java.util.List;
 
-public interface TennisCourtRepository extends JpaRepository<TennisCourt, Long> {
+@Repository
+public interface ReservationRepository extends JpaRepository<Reservation, Long> {
+List<Reservation> findBySchedule_Id(Long scheduleId);
+List<Reservation> findByReservationStatusAndSchedule_StartDateTimeGreaterThanEqualAndSchedule_EndDateTimeLessThanEqual(ReservationStatus reservationStatus, LocalDateTime startDateTime, LocalDateTime endDateTime);
 }
