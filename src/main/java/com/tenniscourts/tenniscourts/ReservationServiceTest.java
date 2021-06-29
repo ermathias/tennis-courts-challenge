cadc4fb34aa42778e1e8ce811ea8ebe8fb3cefff
alexadz96 <86671204+alexadz96@users.noreply.github.com>
2021-06-29 19:11:00 +0300

    Create ReservationServiceTest.java

---
+++ src/main/java/com/tenniscourts/tenniscourts/ReservationServiceTest.java
@@ -0,0 +1,46 @@
+package com.tenniscourts.reservations;
+
+import com.tenniscourts.schedules.Schedule;
+import org.junit.Assert;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
+import org.mockito.InjectMocks;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+@SpringBootTest
+@RunWith(MockitoJUnitRunner.class)
+@ContextConfiguration(classes = ReservationService.class)
+public class ReservationServiceTest {
+
+    @InjectMocks
+    ReservationService reservationService;
+
+    @Test
+    public void getRefundValueFullRefund() {
+        Schedule schedule = new Schedule();
+
+        LocalDateTime startDateTime = LocalDateTime.now().plusDays(2);
+
+        schedule.setStartDateTime(startDateTime);
+
+        Assert.assertEquals(reservationService.getRefundValue(Reservation.builder().schedule(schedule).value(new BigDecimal(10L)).build()), new BigDecimal(10));
+    }
+    
+    public void rescheduleReservation() {
+     System.out.println("***** rescheduleReservation() *****" );
+     try {
+    	 ReservationDTO dto = reservationService.rescheduleReservation( Long.valueOf(1), Long.valueOf(2));
+     }catch (Throwable e) {
+    	 System.out.println("Error: rescheduleReservation() : " + e.toString());
+     }
+     
+    }
+}
